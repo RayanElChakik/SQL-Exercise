7b34e82deae60d986ec5ada8b0eb0fa82be9a921
RayanElChakik <rayan.elchakik@gmail.com>
2021-12-12 20:23:08 +0000

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,8 @@
+SELECT name FROM students ;
+SELECT * FROM students WHERE Age>30;
+SELECT name FROM students WHERE Age=30 AND Gender="F";
+SELECT Points FROM students WHERE Name="Alex";
+INSERT INTO students VALUES(7,"Rayan",25,"M",500);
+UPDATE students SET Points=Points+1 WHERE name="Basma" ;
+UPDATE students SET Points=Points-1 WHERE name="Alex" ;
+
